67c999eb5541f8093047fc4e5030e51e5cce2f63
pranjulr <pranjulrastogi@gmail.com>
2026-03-03 19:27:45 +0000

    container config done

---
+++ .devcontainer/01-basics/01-variables.js
@@ -0,0 +1,18 @@
+const accountId = 12345
+
+let accountEmail = "gmail.com"
+
+var accountPassword = "212121"
+
+accountCity = "Bengaluru"
+
+let accountState;
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Prefer not to use var
+because of issues in block scope and functional scope
+*/
---
+++ .devcontainer/01-basics/02-dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as newer version
+
+let age = 53
+
+//number => 2 to pwer 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined 
+//symbol => unique
+
+//object
+
+console.log(typeof "Pranjul");
+
+console.log(typeof age);
