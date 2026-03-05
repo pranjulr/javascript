6f1848e68d7cf7ea165d6ec6b13dc59c34a75706
pranjulr <pranjulrastogi@gmail.com>
2026-03-05 18:55:44 +0000

    conversion of datatype

---
+++ .devcontainer/01-basics/03-datatypeConversion.js
@@ -0,0 +1,13 @@
+let score = "33"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
