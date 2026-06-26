d5e0ac075777f5b2c7c55a4947eaf6328a7e8d49
pranjulr <pranjulrastogi@gmail.com>
2026-06-26 05:27:53 +0000

    nums and maths

---
+++ .devcontainer/01-basics/06-nums_and_maths.js
@@ -0,0 +1,30 @@
+const score = 400
+
+console.log(score);
+
+balance = new Number(1000); // Number Object
+
+console.log(balance);
+
+console.log(balance.toString());
+
+console.log(balance.toFixed(2)); // toFixed() method formats a number using fixed-point notation.
+
+const otherNumber = 120.8669
+
+console.log(otherNumber.toPrecision(3)); // toPrecision() method formats a number to a specified length.
+
+const hundreds = 1000000
+
+console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++Maths++++++++++++++++++++
+
+console.log(Math);
+
+console.log(Math.abs(-5)); // Absolute value
+
+console.log(Math.round(4.33));
+
+console.log(Math.random());
+console.log(Math.random());
